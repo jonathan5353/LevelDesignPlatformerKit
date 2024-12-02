c8eb640761346d9531ce83d2e2dc7bc465c6a2a6
jonathan5353 <144666821+jonathan5353@users.noreply.github.com>
2024-12-02 17:40:11 +0100

    rotat9ing cylinder

--- rotating_cylinder.gd
+++ rotating_cylinder.gd
@@ -1,17 +1,19 @@
-extends AnimatableBody3D
+extends Node3D
+@onready var spinnng_cylinder: Node3D = $"."
 
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@export var ROTATIONSPEED = 1
+@onready var collision_shape_3d: CollisionShape3D = $AnimatableBody3D/CollisionShape3D
+@onready var spinning_cilinder: AnimatableBody3D = $"."
 
-# The speed at which the cylinder will rotate (degrees per second)
-var rotation_speed: float = 55.0
 
-func _ready():
-	# Initial rotation to lay the cylinder down flat
-	rotation_degrees = Vector3(0, 90, 0)  # Lay down on the ground
 
-func _process(delta):
-	# Rotate the cylinder around the Y-axis each frame
-	rotation_degrees.y += rotation_speed * delta
 
-	# Debug: Print the rotation to confirm it's changing
-	print("Rotation degrees: ", rotation_degrees)
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _process(delta: float) -> void:
+
+	spinning_cilinder.rotation.y += delta * ROTATIONSPEED
