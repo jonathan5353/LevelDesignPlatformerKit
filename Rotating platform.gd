7f73ceedf5259ec740c9c4b52e4012f59d018cae
jonathan5353 <jonathankihlstrom03@gmail.com>
2024-12-11 14:00:43 +0100

    Level stuff

--- Rotating platform.gd
+++ Rotating platform.gd
@@ -1,7 +1,11 @@
 extends Node3D
 @onready var spinnemama: Node3D = $"."
 
-@export var ROTATIONSPEED = 0.5
+@export var ROTATIONSPEED = 2
+
+
+
+
 
 
 @onready var collision_shape_3d: CollisionShape3D = $AnimatableBody3D/CollisionShape3D
